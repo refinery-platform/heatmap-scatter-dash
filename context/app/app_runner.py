355eb7d163056595afa1226bd88bd577a0b48440
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-12-01 08:56:35 -0500

    happy tests

--- context/app/app_runner.py
+++ context/app/app_runner.py
@@ -1,22 +1,24 @@
 import argparse
+import re
 from sys import exit
 
 import numpy as np
 import pandas
-
-import re
 
 from app_factory import make_app
 
 if __name__ != '__main__':
     raise Exception('Should be run as script')
 
+
 def dimensions_regex(s, pattern=re.compile(r"\d+,\d+,\d+")):
     if not pattern.match(s):
         raise argparse.ArgumentTypeError(
-            'Should be of the form "FRAMES,ROWS,COLS", where each is an integer'
+            'Should be of the form "FRAMES,ROWS,COLS", '
+            'where each is an integer'
         )
     return [int(i) for i in s.split(',')]
+
 
 parser = argparse.ArgumentParser(description='Plotly Dash visualization')
 parser.add_argument('--demo', type=dimensions_regex)
@@ -43,13 +45,20 @@ else:
     dataframes = []
     for f in range(frames):
         array = np.random.rand(rows, cols)
-        col_labels = ['cond-{}'.format(i + f * cols // 3) for i in range(cols)]
-        row_labels = ['gene-{}'.format(i + f * rows // 3) for i in range(rows)]
-        dataframes.append(pandas.DataFrame(array, columns=col_labels, index=row_labels))
+        col_labels = ['cond-{}'.format(i + f * cols // 3)
+                      for i in range(cols)]
+        row_labels = ['gene-{}'.format(i + f * rows // 3)
+                      for i in range(rows)]
+        dataframes.append(pandas.DataFrame(array,
+                                           columns=col_labels,
+                                           index=row_labels))
 
 merged_df = pandas.DataFrame()
 for frame in dataframes:
-    merged_df = merged_df.merge(frame, how='outer', right_index=True, left_index=True)
+    merged_df = merged_df.merge(frame,
+                                how='outer',
+                                right_index=True,
+                                left_index=True)
 make_app(merged_df).run_server(
     debug=args.debug,
     port=int(args.port or '8050'),
