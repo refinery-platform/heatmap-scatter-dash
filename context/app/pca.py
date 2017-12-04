e330a6ff33ed3462490e2ac41286671502207b2a
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-12-04 14:15:52 -0500

    pca

---
+++ context/app/pca.py
@@ -0,0 +1,12 @@
+from sklearn import decomposition
+import pandas
+
+def pca(dataframe, components=2):
+    orig_columns = dataframe.columns.tolist()
+    pca = decomposition.PCA(n_components=components)
+
+    array = pca.fit_transform(dataframe.T).tolist()
+    return pandas.DataFrame(
+        array,
+        columns=['pc{}'.format(i) for i in range(components)],
+        index=orig_columns)
---
+++ context/tests/test_pca.py
@@ -0,0 +1,30 @@
+import unittest
+
+import pandas
+from app.pca import pca
+
+
+class TestPCA(unittest.TestCase):
+
+    def test_pca(self):
+        dataframe = pandas.DataFrame(
+            [[1, 2, 8, 9],
+             [1, 2, 8, 9],
+             [1, 2, 8, 9],
+             [9, 1, 9, 1]],
+            columns=['c1', 'c2', 'c3', 'c4'],
+            index=['r1', 'r2', 'r3', 'r4']
+        )
+        principle_components = pca(dataframe)
+        self.assertEqual(principle_components.round().as_matrix().tolist(),
+            [[7.0, 3.0],
+             [5.0, -5.0], # The pairs 1+2 and 3+4 are similar in the first component
+             [-5.0, 5.0], # But 1+3 and 2+4 are also related, as shown in the second component
+             [-7.0, -3.0]]
+        )
+        self.assertEqual(principle_components.columns.tolist(),
+            ['pc0', 'pc1']
+        )
+        self.assertEqual(principle_components.index.tolist(),
+            ['c1', 'c2', 'c3', 'c4']
+        )
