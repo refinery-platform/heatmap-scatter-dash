51e24f4e1d9a015b78239a1aed3ad93285349584
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-12-04 16:19:28 -0500

    Fix tests... but Docker fails because PCA on NaN does not work

--- context/tests/test_dash.py
+++ context/tests/test_dash.py
@@ -29,7 +29,7 @@ class TestDash(unittest.TestCase):
         self.assertEqual(
             tree(self.app.layout),
             [
-                ['Graph', 'str'],
+                ['Graph', 'Graph'],
                 [['Graph', ['Input'], ['Dropdown'], ['Dropdown']], 'str']
             ]
         )
@@ -37,7 +37,7 @@ class TestDash(unittest.TestCase):
     def test_callback_map(self):
         self.assertEqual(
             list(self.app.callback_map.keys()),
-            ['scatter.figure', 'heatmap.figure']
+            ['heatmap.figure', 'scatter-pca.figure', 'scatter.figure']
         )
 
     # Outside resource tests:
