ee78cda4e259559acf24a00156bf8bdec8c5dca3
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-12-07 10:27:17 -0500

    Checkpoint: new cli args, but not structured well

--- context/app_runner.py
+++ context/app_runner.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas
 
 from app.app_wrapper import AppWrapper
+from app.dataframe_merge import DataFrameMerge
 
 
 def dimensions_regex(s, pattern=re.compile(r"\d+,\d+,\d+")):
@@ -44,27 +45,33 @@ def demo_dataframes(frames, rows, cols):
 
 
 def main(args, parser=None):
-    if args.files:
-        dataframes = real_dataframes(args.files)
-    elif args.demo:
-        dataframes = demo_dataframes(**args.demo)
+    if args.counts:
+        count_frames = real_dataframes(args.counts)
+    elif args.demo_counts:
+        count_frames = demo_dataframes(**args.demo_counts)
     else:
-        message = 'Either "--demo FRAMES,ROWS,COLS" '\
-                  'or "--files FILE" is required'
-        if parser:
-            print(message)
-            parser.print_help()
-            exit(1)
-        else:
-            raise Exception(message)
+        raise Exception('argparse validation not working?')
 
+    if args.diffs:
+        diff_frames = real_dataframes(args.diffs)
+    elif args.demo_diffs:
+        diff_frames = demo_dataframes(**args.demo_diffs)
+    else:
+        diff_frames = []
+
+    df_merge = DataFrameMerge(
+        count_frames=count_frames,
+        diff_frames=diff_frames,
+        cluster=args.cluster)
+
+    # TODO: move
     merged_df = pandas.DataFrame()
-    for frame in dataframes:
+    for frame in count_frames:
         merged_df = merged_df.merge(frame,
                                     how='outer',
                                     right_index=True,
                                     left_index=True)
-    AppWrapper(merged_df, clustering=args.cluster).app.run_server(
+    AppWrapper(df_merge).app.run_server(
         debug=args.debug,
         port=args.port,
         host='0.0.0.0'
@@ -73,10 +80,17 @@ def main(args, parser=None):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Plotly Dash visualization')
-    input_source = parser.add_mutually_exclusive_group(required=True)
-    input_source.add_argument('--demo', type=dimensions_regex)
-    input_source.add_argument('--files', nargs='+',
+
+    counts_source = parser.add_mutually_exclusive_group(required=True)
+    counts_source.add_argument('--demo_counts', type=dimensions_regex)
+    counts_source.add_argument('--counts', nargs='+',
                               type=argparse.FileType('r'))
+
+    diffs_source = parser.add_mutually_exclusive_group()
+    diffs_source.add_argument('--demo_diffs', type=dimensions_regex)
+    diffs_source.add_argument('--diffs', nargs='+',
+                               type=argparse.FileType('r'))
+
     parser.add_argument('--port', type=int, default=8050)
     parser.add_argument('--debug', action='store_true')
     parser.add_argument('--cluster', action='store_true')
