f22e4383200a0ac8e0d02ceb3ca1690cae477b22
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-12-07 11:56:51 -0500

    checkpoint: begin clustering

--- context/tests/test_data_frame_merge.py
+++ context/tests/test_data_frame_merge.py
@@ -26,3 +26,15 @@ class TestDataFrameMerge(unittest.TestCase):
     def test_no_change(self):
         merged_frame = DataFrameMerge(self.dataframes).count_frame
         self.assertEqualDataFrames(merged_frame, self.dataframes[0])
+
+    def test_cluster(self):
+        # Anything more than this should be tested in test_cluster.py
+        merged_frame = DataFrameMerge(self.dataframes, clustering=True).count_frame
+        self.assertEqualDataFrames(merged_frame, pandas.DataFrame([
+                [1, 1, 4, 5],
+                [2, 2, 4, 5],
+                [8, 8, 4, 5],
+                [9, 9, 4, 5]],
+            columns=['c1', 'c3', 'c2', 'c4'],
+            index=['r1', 'r3', 'r2', 'r4']
+        ))
