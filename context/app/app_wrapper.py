5ae427c07d6d4deebfa4b3348eb8f4d01a6b593e
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-12-11 14:36:18 -0500

    separation of concerns: move layout to own file

--- context/app/app_wrapper.py
+++ context/app/app_wrapper.py
@@ -1,8 +1,4 @@
-from base64 import urlsafe_b64encode
-
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
 import plotly.graph_objs as go
 from dash.dependencies import Input, Output
 from plotly.figure_factory.utils import (PLOTLY_SCALES, label_rgb, n_colors,
@@ -10,6 +6,7 @@ from plotly.figure_factory.utils import (PLOTLY_SCALES, label_rgb, n_colors,
 
 from app.cluster import cluster
 from app.pca import pca
+from app.app_layout import configure_layout
 
 
 def _log_interpolate(color_scale):
@@ -29,13 +26,6 @@ def _log_interpolate(color_scale):
     return [[0, label_rgb(interpolated[points - 1])]] + missing_zero
 
 
-def _to_data_uri(s):
-    uri = (
-        'data:application/javascript;base64,'.encode('utf8') +
-        urlsafe_b64encode(s.encode('utf8'))
-    ).decode("utf-8", "strict")
-    return uri
-
 
 class AppWrapper:
 
@@ -45,154 +35,10 @@ class AppWrapper:
         self._conditions = self._dataframe.axes[1].tolist()
         self._color_scale = _log_interpolate(PLOTLY_SCALES[colors])
         self.app = dash.Dash()
-        self._configure_layout()
+        configure_layout(self)
         self._configure_callbacks()
 
-    def _configure_layout(self):
-        self.app.css.append_css({
-            'external_url':
-                'https://maxcdn.bootstrapcdn.com/'
-                'bootstrap/3.3.7/css/bootstrap.min.css'
-        })
-        self.app.scripts.append_script({
-            'external_url':
-            'https://code.jquery.com/jquery-3.1.1.slim.min.js'
-        })
-        self.app.scripts.append_script({
-            'external_url':
-                'https://maxcdn.bootstrapcdn.com/'
-                'bootstrap/3.3.7/js/bootstrap.min.js'
-        })
-        self.app.scripts.append_script({
-            'external_url':
-                'https://maxcdn.bootstrapcdn.com/'
-                'bootstrap/3.3.7/js/bootstrap.min.js'
-        })
-        self.app.scripts.append_script({
-            'external_url': _to_data_uri("""
-                $('body').on('click', '.nav-tabs a', function(e) {
-                    e.preventDefault();
-                    $(this).tab('show');
-                });""")
-            # TODO: This is not good.
-            # Currently, there is no way to get data attributes in Dash.
-            # https://community.plot.ly/t/can-data-attributes-be-created-in-dash/7222
-            # So, we need to register them by hand...
-            # but when the JS loads, React hasn't yet
-            # generated the DOM, so we use "on" instead.
-        })
 
-        conditions_options = [
-            {'label': cond, 'value': cond}
-            for cond in self._conditions
-        ]
-
-        pc_options = [
-            {'label': pc, 'value': pc}
-            for pc in ['pc1', 'pc2', 'pc3', 'pc4']  # TODO: DRY
-        ]
-
-        def dropdown(id, options, axis, axis_index):
-            return html.Span(
-                [
-                    html.Label(
-                        [axis],
-                        className='col-sm-1 control-label'
-                    ),
-                    dcc.Dropdown(
-                        id='scatter-{}-{}-axis-select'.format(id, axis),
-                        options=options,
-                        value=options[axis_index]['value'],
-                        className='col-sm-5'
-                    )
-                ]
-            )
-
-        def scatter(id, options, search=False, log=False):
-            control_nodes = [
-                html.Div([
-                    dropdown(id, options, 'x', 0),
-                    dropdown(id, options, 'y', 1)
-                ],
-                    className='form-group')
-            ]
-            if search:
-                control_nodes.append(html.Div([
-                    html.Label(
-                        ['gene'],
-                        className='col-sm-1 control-label'
-                    ),
-                    html.Div([
-                        dcc.Input(
-                            id='search-{}'.format(id),
-                            placeholder='search...',
-                            type="text",
-                            className='form-control')
-                    ],
-                        className='col-sm-11')
-                ],
-                    className='form-group'))
-            nodes = [
-                dcc.Graph(
-                    id='scatter-{}'.format(id),
-                    style={'height': '33vh'}
-                ),
-                html.Div(control_nodes, className='form-horizontal')
-            ]
-            return html.Div(nodes, className='tab-pane active', id=id)
-
-        self.app.layout = html.Div([
-            html.Div([
-                html.Div([
-                    dcc.Graph(
-                        id='heatmap',
-                        style={'height': '100vh'}
-                    ),
-                ],
-                    className='col-md-6'),
-                html.Div([
-                    html.Br(),  # Top of tab was right against window top
-                    html.Ul([
-                        html.Li([
-                            html.A([
-                                'PCA'
-                            ],
-                                href='#pca'
-                            )
-                        ], className='active')
-                    ],
-                        className='nav nav-tabs'),
-                    html.Div([
-                        scatter('pca', pc_options),
-                    ]),
-                    html.Ul([
-                        html.Li([
-                            html.A([
-                                'Genes'
-                            ],
-                                href='#genes'
-                            )
-                        ],
-                            className='active'),
-                        html.Li([
-                            html.A([
-                                'Volcano'
-                            ],
-                                href='#volcano')
-                        ])
-                    ],
-                        className='nav nav-tabs'),
-                    html.Div([
-                        scatter('genes', conditions_options, search=True),
-                        scatter('volcano', conditions_options)
-                    ],
-                        className='tab-content')
-                ],
-                    className='col-md-6')
-            ],
-                className='row')
-        ],
-            className='container')
 
     def _configure_callbacks(self):
         genes = self._dataframe.axes[0].tolist()
