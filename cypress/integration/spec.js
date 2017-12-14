b355797799e77d1e664ef7872da298d7d9580e01
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-12-14 15:26:18 -0500

    A little more cypress

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -2,5 +2,13 @@ describe('App', function(){
   it('should have a title', function() {
     cy.visit('http://localhost:8888/');
     cy.title().should('equal', 'Heatmap + Scatterplots');
+    cy.contains('Volcano').click();
+
+    cy.get('body').should('not.contain', 'linear');
+    cy.contains('log').parent().click();
+    cy.get('body').should('contain', 'linear').click();
+
+    // cy.get('body').should('not.contain', 'log');
+    // cy.contains('linear').parent().click();
   });
 });
