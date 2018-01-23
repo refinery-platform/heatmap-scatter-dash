ee1293ce0b1c6ffaab9865e8b06a044bb45e9e4e
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-01-23 14:34:02 -0500

    Make file vs path more clear

--- context/app/utils/tabular.py
+++ context/app/utils/tabular.py
@@ -1,12 +1,13 @@
-from pandas import read_csv
 from csv import Sniffer
 
+from pandas import read_csv
 
-def parse(file_path, col_zero_index=True):
-    with open(file_path) as file_handle:
-        dialect = Sniffer().sniff(file_handle.readline())
+
+def parse(file, col_zero_index=True):
+    dialect = Sniffer().sniff(file.readline())
+    file.seek(0)
     return read_csv(
-        file_path,
+        file,
         index_col=0 if col_zero_index else None,
         delimiter=dialect.delimiter,
         quotechar=dialect.quotechar,
