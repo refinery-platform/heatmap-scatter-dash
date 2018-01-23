649cf0f74968a1351be718172a3f17fd88433f80
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-01-23 14:54:44 -0500

    Pass in dialect object itself

--- context/app/utils/tabular.py
+++ context/app/utils/tabular.py
@@ -1,3 +1,4 @@
+import warnings
 from csv import Sniffer
 
 from pandas import read_csv
@@ -6,10 +7,13 @@ from pandas import read_csv
 def parse(file, col_zero_index=True):
     dialect = Sniffer().sniff(file.readline())
     file.seek(0)
-    return read_csv(
-        file,
-        index_col=0 if col_zero_index else None,
-        delimiter=dialect.delimiter,
-        quotechar=dialect.quotechar,
-        # TODO: more?
-    )
+    with warnings.catch_warnings():
+        # https://github.com/pandas-dev/pandas/issues/18845
+        # pandas raises unnecessary warnings.
+        warnings.simplefilter("ignore")
+        dataframe = read_csv(
+            file,
+            index_col=0 if col_zero_index else None,
+            dialect=dialect
+        )
+    return dataframe
