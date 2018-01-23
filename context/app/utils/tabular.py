2ffd20c1f58cad51bfffd10d436195227e618d64
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-01-23 14:04:24 -0500

    Just sniff the first line, and use the default c parser: better than twice as fast

--- context/app/utils/tabular.py
+++ context/app/utils/tabular.py
@@ -1,6 +1,14 @@
 from pandas import read_csv
+from csv import Sniffer
 
 
-def parse(file, col_zero_index=True):
-    index_col = 0 if col_zero_index else None
-    return read_csv(file, index_col=index_col, sep=None, engine='python')
+def parse(file_path, col_zero_index=True):
+    with open(file_path) as file_handle:
+        dialect = Sniffer().sniff(file_handle.readline())
+    return read_csv(
+        file_path,
+        index_col=0 if col_zero_index else None,
+        delimiter=dialect.delimiter,
+        quotechar=dialect.quotechar,
+        # TODO: more?
+    )
