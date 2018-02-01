832394584600fe346e6cdb186e05aec862e0ad5a
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-02-01 09:55:06 -0500

    TODOs

--- context/start-eb.sh
+++ context/start-eb.sh
@@ -2,6 +2,7 @@
 set -o errexit
 
 eb init
-pipenv lock --requirements > requirements.txt  # gitignored
+echo '# Do not edit by hand. / Do not check in.' > requirements.txt
+pipenv lock --requirements >> requirements.txt  # gitignored
 eb create heatmap-scatterplot
 eb status | grep CNAME | sed 's/CNAME: /http:\/\//'
