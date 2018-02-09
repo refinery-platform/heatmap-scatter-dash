d737406d32eec862390252d90e53bbed5b730b17
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-02-09 18:01:42 -0500

    This feels gratuitous now... but maybe it is useful?

--- test.sh
+++ test.sh
@@ -21,6 +21,16 @@ isort --recursive context --check-only || die "Run 'isort --recursive context'"
 end isort
 
 
+start pip
+# TODO: Uncomment this once requirements-freeze.txt is in master
+#URL_BASE=https://raw.githubusercontent.com/refinery-platform/heatmap-scatter-dash/master
+# ! ( diff <(cat context/requirements.txt; cat requirements-dev.txt) \
+#         <(curl --silent $URL_BASE/context/requirements.txt; curl --silent $URL_BASE/requirements-dev.txt) \
+#    || diff requirements-freeze.txt <(curl --silent $URL_BASE/requirements-freeze.txt) ) ||
+#die 'If non-frozen requirements files change, so should the frozen one, and vice versa.'
+end pip
+
+
 start usage
 diff <(perl -ne 'print if /^usage:/../^  --api_prefix/' README.md) <(cd context; ./app_runner.py -h) || \
 die '
