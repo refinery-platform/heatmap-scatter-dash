f691851f7aa993098b29d306da2eb8023926659a
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-02-13 19:47:07 -0500

    Mostly just testing tabs

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,18 +1,44 @@
-describe('App', function(){
-  it('dash app works', function() {
+describe('heatmap-scatterplot', function(){
+  it('dash (heatmap) works', function() {
     cy.visit('http://localhost:8888/');
     cy.title().should('equal', 'Heatmap + Scatterplots');
     cy.location('search').should('equal',
         '?scale=log&palette=black-white&cluster-rows=cluster'
-        +'&cluster-cols=cluster&label-rows=auto&label-cols=auto');
-
-    // TODO: Test heatmap interactions
-
+        + '&cluster-cols=cluster&label-rows=auto&label-cols=auto');
+    cy.get('#heatmap svg').should('be.visible');
     cy.get('#search-genes').click(); // TODO: type() fails
+  });
 
-    // TODO: Test sample-by-sample plot
+  it('dash (top tabs) work', function() {
+    cy.visit('http://localhost:8888/');
+    cy.get('#pca svg').should('be.visible');
 
+    cy.get('#ids-iframe').should('not.be.visible');
+    cy.contains('IDs').click();
+    cy.get('#pca svg').should('not.be.visible');
+    cy.get('#ids-iframe').should('be.visible');
+
+    cy.contains('Options').click();
+    cy.get('#ids-iframe').should('not.be.visible');
+    cy.contains('.Select', 'log').click();
+    cy.contains('.Select', 'black-white').click();
+    cy.contains('.Select', 'cluster').click();
+    cy.contains('.Select', 'auto').click();
+    // TODO: Change selection: They are not really selects, so this is hard?
+
+    cy.contains('a', 'Help').click();
+    // Can't actually visit new page:
+    // https://docs.cypress.io/faq/questions/using-cypress-faq.html#Can-I-test-anchor-links-that-open-in-a-new-tab
+
+  });
+
+  it('dash (bottom tabs) work', function() {
+    cy.visit('http://localhost:8888/');
+    cy.get('#sample-by-sample svg').should('be.visible');
+
+    cy.get('#volcano svg').should('not.be.visible');
     cy.contains('Volcano').click();
+    cy.get('#volcano svg').should('be.visible');
     // TODO: Test volcano plot
 
     cy.contains('.Select', 'log_fold_change').click();
@@ -29,24 +55,9 @@ describe('App', function(){
     cy.contains('List').click();
     cy.get('#list-iframe').should('be.visible');
     // TODO: Test content of iframe
-
-    cy.get('#ids-iframe').should('not.be.visible');
-    cy.contains('IDs').click();
-    cy.get('#ids-iframe').should('be.visible');
-
-    cy.contains('Options').click();
-    cy.contains('.Select', 'log').click();
-    cy.contains('.Select', 'black-white').click();
-    cy.contains('.Select', 'cluster').click();
-    cy.contains('.Select', 'auto').click();
-    // TODO: Change selection
-
-    cy.contains('a', 'Help').click();
-    // Can't actually visit new page:
-    // https://docs.cypress.io/faq/questions/using-cypress-faq.html#Can-I-test-anchor-links-that-open-in-a-new-tab
   });
 
-  it('help app works', function() {
+  it('help works', function() {
     cy.visit('http://localhost:8888/help');
     cy.title().should('equal', 'Heatmap + Scatterplots: Help');
     cy.contains('This is a tool for exploring');
