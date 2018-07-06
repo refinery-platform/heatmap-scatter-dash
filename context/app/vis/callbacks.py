ddbd55f085055b0344768ce65402f1ea48728437
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-07-06 17:55:49 -0400

    Volcano now works

--- context/app/vis/callbacks.py
+++ context/app/vis/callbacks.py
@@ -124,14 +124,19 @@ class VisCallbacks(VisGeneCallbacks, VisConditionCallbacks):
         # With a proportional font, this is only an estimate.
         char_width = 10
 
+        # We do not coerce the dataframe indexes to be strings:
+        # Doing so makes no difference to Dash, which still requires
+        # an explicit type="category", and it causes problems in vulcanize.
+        # So we call str() explicitly below.
+
         if show_genes:
-            row_max = max([len(s) for s in list(cluster_dataframe.index)])
+            row_max = max([len(str(s)) for s in list(cluster_dataframe.index)])
             left_margin = row_max * char_width
         else:
             left_margin = 75
 
         if show_conditions:
-            col_max = max([len(s) for s in list(cluster_dataframe)])
+            col_max = max([len(str(s)) for s in list(cluster_dataframe)])
             bottom_margin = col_max * char_width
         else:
             bottom_margin = 10
