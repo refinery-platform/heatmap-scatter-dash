b4549cac2455f7e4794edc913517084d8faf3c69
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-08-12 16:13:29 -0400

    list of REs rather than single one

--- context/app/utils/vulcanize.py
+++ context/app/utils/vulcanize.py
@@ -1,8 +1,8 @@
 import re
 from math import log10
 
-LOG_FOLD_RE = r'\blog[^a-z]'
-P_VALUE_RE = r'p.*value'
+LOG_FOLD_RE_LIST = [r'\blog[^a-z]']
+P_VALUE_RE_LIST = [r'p.*value']
 
 
 def vulcanize(dataframe):
@@ -12,8 +12,8 @@ def vulcanize(dataframe):
     remove all other columns,
     and replace p-value with its negative log.
     """
-    log_fold_col = _pick_col(LOG_FOLD_RE, dataframe)
-    p_value_col = _pick_col(P_VALUE_RE, dataframe)
+    log_fold_col = _pick_col(LOG_FOLD_RE_LIST, dataframe)
+    p_value_col = _pick_col(P_VALUE_RE_LIST, dataframe)
     log_p_value_col = '-log10({})'.format(p_value_col)
     dataframe[log_p_value_col] =\
         dataframe[p_value_col].apply(_neg_log)
@@ -23,15 +23,17 @@ def vulcanize(dataframe):
     # (I think.)
 
 
-def _pick_col(name_re, df):
-    match_cols = [
-        col for col in df.columns
-        if re.search(name_re, col, flags=re.IGNORECASE)
-    ]
-    assert len(match_cols) == 1, \
-        'expected one match for /{}/i in {}, got {}'.format(
-            name_re, df.columns.tolist(), match_cols
-    )
+def _pick_col(name_re_list, df):
+    for name_re in name_re_list:
+        match_cols = [
+            col for col in df.columns
+            if re.search(name_re, col, flags=re.IGNORECASE)
+        ]
+        if len(match_cols) == 1:
+            break
+    else:
+        raise Exception('expected one match for {} in {}'.format(
+            name_re_list, df.columns.tolist()))
     return match_cols[0]
 
 
